3354dc609f7dabd20f4a973ff8cb9dc3488a3ca2
Gurbakhshish Singh <gurbakhshish.s@gmail.com>
2020-10-25 21:34:42 -0400

    adding clap as dep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Clap;
+
+#[derive(Clap)]
+#[clap(version="1.0", author="Gurbakhshish Singh")]
+struct Opts{
+
+    #[clap(short, long, default_value="config.yaml")]
+    config: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+    println!("{config_file_name}", config_file_name = opts.config)
 }
